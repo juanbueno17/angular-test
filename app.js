b2d88613a7c2af67cbb345cade467e4b85ccb065
juanbueno <juanbueno1711@gmail.com>
2018-02-19 23:00:44 -0500

    algunas funciones

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 (function () {
   'use strict';
   var x = "hola";
-  var aplicacion = angular.module('miPrimeraAplicacion', [])
-  aplicacion.controller('miPrimerControlador', function($scope){
+
+  var aplicacion = angular.module('DIApp', [])
+
+  /*aplicacion.controller('miPrimerControlador', function($scope){
     $scope.name = "JuanK";
     $scope.sayHello = function(){
       return $scope.name.length;
     }
-  })
+  })*/
+
+  aplicacion.controller('DIController', DIController);
+  function DIController($scope, $filter, $injector){
+    $scope.name = "JuanK2";
+    $scope.upper = function(){
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    };
+
+    console.console.log($injector.AnnonateMe(DIController));
+  }
+
+  function AnnonateMe(name, job, blah){
+    return "Blah!";
+  }
+
+  console.log(DIController.toString());
 })();
